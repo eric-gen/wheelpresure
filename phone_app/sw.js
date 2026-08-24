f6299f936612464e2f2502d8ba6f07d850236e49
Marc <generic@vaengineering.be>
2026-08-24 17:38:59 +0200

    onthoud gekoppelde boards en drukwaarden na herstart (getDevices + localStorage)

--- phone_app/sw.js
+++ phone_app/sw.js
@@ -1,5 +1,5 @@
 /* Tire Pressure PWA - cache-first offline shell */
-const CACHE='tire-pwa-v6';
+const CACHE='tire-pwa-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{

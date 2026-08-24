c335fa5cb6ca4e58b2257e30f81c803a882873a6
Marc <generic@vaengineering.be>
2026-08-24 18:03:56 +0200

    diagnose waarom bluetooth-geheugen ontbreekt (api afwezig vs fout)

--- phone_app/sw.js
+++ phone_app/sw.js
@@ -1,5 +1,5 @@
 /* Tire Pressure PWA - cache-first offline shell */
-const CACHE='tire-pwa-v9';
+const CACHE='tire-pwa-v10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
